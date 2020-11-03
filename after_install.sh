e876f7ce6d4744ef441c199a09596804ceff33ea
Danila <dghak@bk.ru>
2020-11-03 17:15:45 +0300

    Script improvements

--- after_install.sh
+++ after_install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-xdg-user-dirs-update
+ehco "Setting timezone and time sync..."
 timedatectl set-timezone Europe/Moscow
 timedatectl set-ntp true
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &> /dev/null
 git config --global user.name "Danila"
 git config --global user.email "dghak@bk.ru"
-yay -S translate-shell zoom jmtpfs swaylock-effects-git yandex-disk spotify adbfs-rootless-git intellij-idea-ce scrcpy nm-connection-editor networkmanager-openvpn hunspell hunspell-en_US hunspell-ru-aot-ieyo hyphen hyphen-en hyphen-ru ytop-bin --noconfirm
+echo "Installing additiional packages..."
+yay -S translate-shell zoom jmtpfs swaylock-effects-git yandex-disk spotify adbfs-rootless-git scrcpy nm-connection-editor networkmanager-openvpn hunspell hunspell-en_US hunspell-ru-aot-ieyo hyphen hyphen-en hyphen-ru ytop-bin --noconfirm
+echo "Zoom configuration..."
 mkdir ~/.local/share/applications
 cp /usr/share/applications/Zoom.desktop ~/.local/share/applications
 sed -i 's+Exec=/usr/bin/zoom %U+Exec=env QT_QPA_PLATFORM=xcb /usr/bin/zoom %U+g' ~/.local/share/applications/Zoom.desktop
-git clone https://aur.archlinux.org/tor-browser.git
+echo "Installing Tor Browser..."
+git clone https://aur.archlinux.org/tor-browser.git &> /dev/null
 cd tor-browser
-gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org
-makepkg -si --noconfirm
+gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org &> /dev/null
+makepkg -si --noconfirm &> /dev/null
 cd ..
 rm -rf tor-browser
-update-desktop-database ~/.local/share/applications
+update-desktop-database ~/.local/share/applications &> /dev/null
+echo "Finished!"
---
+++ install_arch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Welcome to the Arch-Sway installing script!"
+read -p 'Enter disk encryption password: ' cryptpass
+read -p 'Enter your hostname (name of your PC): ' hsname
+rmmod pcspkr
+timedatectl set-ntp true
+echo "Updating Arch mirrors..."
+reflector --latest 250 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo "Configuring disks..."
+sgdisk /dev/sda -o &> /dev/null
+sgdisk --clear \
+         --new=1:0:+100MiB --typecode=1:ef00 --change-name=1:EFI \
+         --new=2:0:0       --typecode=2:8300 --change-name=2:cryptsystem \
+           /dev/sda &> /dev/null
+mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI &> /dev/null
+echo $cryptpass | cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 /dev/disk/by-partlabel/cryptsystem &> /dev/null
+echo $cryptpass | cryptsetup open /dev/disk/by-partlabel/cryptsystem system &> /dev/null
+mkfs.btrfs --force --label system /dev/mapper/system &> /dev/null
+export o_swap=defaults,X-mount.mkdir,ssd,discard=async,noatime,nodiratime,space_cache
+export o=$o_swap,commit=120,compress=zstd
+mount -t btrfs LABEL=system /mnt &> /dev/null
+btrfs subvolume create /mnt/@ &> /dev/null
+btrfs subvolume create /mnt/@home &> /dev/null
+btrfs subvolume create /mnt/@swap &> /dev/null
+umount -R /mnt &> /dev/null
+mount -t btrfs -o subvol=@,$o LABEL=system /mnt &> /dev/null
+mount -t btrfs -o subvol=@home,$o LABEL=system /mnt/home &> /dev/null
+mount -t btrfs -o subvol=@swap,$o_swap LABEL=system /mnt/swap &> /dev/null
+mount -o X-mount.mkdir LABEL=EFI /mnt/boot &> /dev/null
+echo "Installing packages..."
+pacstrap /mnt base base-devel linux linux-firmware btrfs-progs man-db man-pages neovim networkmanager intel-ucode
+echo "Configuring..."
+genfstab -L -p /mnt >> /mnt/etc/fstab &> /dev/null
+echo $hsname > /mnt/etc/hostname
+echo "127.0.0.1	localhost
+::1		localhost
+127.0.1.1	${hsname}.localdomain	${hsname}" > /mnt/etc/hosts
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/locale.gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf 
+cp s_part.sh /mnt/
+cp after_install.sh /mnt/
+cp btrfs_map_physical.c /mnt/
+cp -r .config /mnt/
+cp -r .local /mnt/
+cp -r Wallpapers /mnt/
+cp -r scripts /mnt/
+chmod +x /mnt/after_install.sh
+chmod +x /mnt/s_part.sh
+arch-chroot /mnt ./s_part.sh
+rm /mnt/s_part.sh
+
+echo "Installation is complete! Now you can reboot to you system. 
+After rebooting launch script after_install.sh placed in your home directory to install some important components.
+You can delete the script after that.
+For finishing neovim configuration type command    :PlugInstall        in nvim"
