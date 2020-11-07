65c78809922de6fcec179b218fcf429968e4d4a1
Danila <dghak@bk.ru>
2020-11-08 01:01:32 +0300

    Minor changes

--- configure_snapshots.sh
+++ configure_snapshots.sh
@@ -1,3 +1,10 @@
+#!/bin/bash                                                                                                                                                                                     
+#Run as root!                                                                                                                                                                                   
+                                                                                                                                                                                                
+if [ "$(whoami)" != "root" ]; then                                                                                                                                                              
+        echo "Script must be run as root!"                                                                                                                                                      
+        exit 255                                                                                                                                                                                
+else       
 umount -R /.snapshots
 umount -R /home/.snapshots
 rm -r /.snapshots
@@ -13,3 +20,4 @@ sed -i 's/TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"/g' /etc/snapper/c
 sed -i 's/TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMIT_MONTHLY="0"/g' /etc/snapper/configs/home
 sed -i 's/TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="2"/g' /etc/snapper/configs/home
 sed -i 's/TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="5"/g' /etc/snapper/configs/home
+fi
