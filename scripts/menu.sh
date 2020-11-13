ee6503937765a75444a3fcd1eacbfacbd2d2757b
Danila <dghak@bk.ru>
2020-11-13 04:37:22 +0300

    UI improvements

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bemenu-run -ni -l 7 -p "Launch: " --fn "DejaVu Sans Mono" --nb "#222222" --tb "#222222" --fb "#222222" --nf "#FFFFFF" --hf "#9CD45B" --tf "#9CD45B" --hb "#424242" --monitor $(/home/$USER/scripts/menu.py)
+pkill bemenu || bemenu-run -ni -l 7 -p "Launch: " --fn "DejaVu Sans Mono" --nb "#222222" --tb "#222222" --fb "#222222" --nf "#FFFFFF" --hf "#9CD45B" --tf "#9CD45B" --hb "#424242" --monitor $(/home/$USER/scripts/menu.py)
