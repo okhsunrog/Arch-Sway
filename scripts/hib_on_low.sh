a5404f081dfb1e420fc7a6bd639b4052de3f0ad5
Danila <dghak@bk.ru>
2020-12-01 03:48:57 +0300

    Hibernation settings changed

--- scripts/hib_on_low.sh
+++ scripts/hib_on_low.sh
@@ -3,10 +3,10 @@
 acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
 	read -r status capacity
 
-	if [ "$status" = Discharging -a "$capacity" -lt 6 ]; then
+	if [ "$status" = Discharging -a "$capacity" -lt 7 ]; then
 		logger "Critical battery threshold"
-    dbus-send --system / net.nuetzlich.SystemNotifications.Notify 'string:Low battery!' 'string:Your system will be hibernated in 60 seconds.'
-    sleep 60
+    dbus-send --system / net.nuetzlich.SystemNotifications.Notify 'string:Low battery!' 'string:Your system will be hibernated in 30 seconds.'
+    sleep 30
 		sudo systemctl hibernate
 	fi
 
