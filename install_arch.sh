0652941986919c7cc958f89ffa70b54cad13b447
Danila <dghak@bk.ru>
2021-02-13 20:58:58 +0300

    Check internet connection before we start

--- install_arch.sh
+++ install_arch.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 rmmod pcspkr
 echo "Welcome to the Arch-Sway installing script!"
-sleep 7
-# Tests
-ls /sys/firmware/efi/efivars > /dev/null && \
-  ping archlinux.org -c 1 > /dev/null &&    \
-  timedatectl set-ntp true > /dev/null &&   \
-  echo "Tests ok"
-
-sleep 7
-
+ping -c 2 archlinux.org || { echo "No internet connection!"; exit; }
+timedatectl set-ntp true
+sleep 1
 read -p 'Enter disk encryption password: ' cryptpass
 read -p 'Enter host name:' hsname
 reflector --verbose --sort rate --protocol https --country Russia --country Germany --age 12 --save /etc/pacman.d/mirrorlist
