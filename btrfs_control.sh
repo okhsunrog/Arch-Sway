ed716abe1b05ba62218e81c0f61b35838e4bcc07
Danila <dghak@bk.ru>
2021-02-14 16:04:16 +0300

    Repare tools updated

--- btrfs_control.sh
+++ btrfs_control.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cryptsetup luksOpen /dev/disk/by-partlabel/cryptsystem system
+cp temp/initramfs* /tmp/exxtr/
+umount temp
+rm -rf temp
+cd /tmp/exxtr
+lsinitcpio -x initramfs*
+cp crypto_keyfile.bin ~/
+rm -rf /tmp/exxtr
+cd ~
 o=commit=120,compress=zstd,defaults,ssd,discard=async,noatime,nodiratime,space_cache              
 umount -R /mnt
 mount -t btrfs LABEL=system /mnt
--- repair.sh
+++ repair.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-cryptsetup luksOpen /dev/disk/by-partlabel/cryptsystem system
+mkdir temp
+mkdir /tmp/exxtr
+mount LABEL=EFI temp
+cp temp/initramfs* /tmp/exxtr/
+umount temp
+rm -rf temp
+cd /tmp/exxtr
+lsinitcpio -x initramfs*
+cp crypto_keyfile.bin ~/
+rm -rf /tmp/exxtr
+cd ~
+cryptsetup --batch-mode --cipher=aes-xts-plain64 --offset=0 --key-file=crypto_keyfile.bin --key-size=512 open --type plain /dev/sda cryptsystem
 o=commit=120,compress=zstd,defaults,ssd,discard=async,noatime,nodiratime,space_cache
 umount -R /mnt
 mount -t btrfs -o subvol=@,$o LABEL=system /mnt
